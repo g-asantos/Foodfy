a1c2f82e1235cf1b11dd515b7ce2baeeb80ff613
Guilherme Santos <gui_t900@hotmail.com>
2020-05-24 21:30:59 -0300

    header test

---
+++ public/hideButton.js
@@ -0,0 +1,59 @@
+const buttons = document.querySelectorAll('h5')
+for (let button of buttons) {
+
+
+  button.addEventListener('click', function () {
+    if (button.innerHTML == 'MOSTRAR') {
+      button.innerHTML = 'ESCONDER';
+      button.nextElementSibling.classList.remove('show')
+
+    } else if (button.innerHTML == 'ESCONDER') {
+      button.innerHTML = 'MOSTRAR';
+      button.nextElementSibling.classList.add('show')
+
+    }
+
+
+  })
+}
+
+
+
+function addIngredient() {
+  const ingredients = document.querySelector("#ingredients");
+  const fieldContainer = document.querySelectorAll(".ingredient");
+
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+
+
+function addPrep() {
+  const preps = document.querySelector("#preparation");
+  const fieldContainer = document.querySelectorAll(".prepare");
+
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = "";
+  preps.appendChild(newField);
+}
+
+
+document.querySelector(".add-ingredient").addEventListener("click", addIngredient);
+
+
+document.querySelector(".add-step").addEventListener("click", addPrep);
+
--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,4 @@
 
- 
-
 const ImageGallery = {
   highlight: document.querySelector('.card__image-container .highlight > img'),
   previews: document.querySelectorAll('.filePreview img'),
@@ -14,64 +12,4 @@ const ImageGallery = {
     ImageGallery.highlight.src = target.src
   }
 }
-
-const buttons = document.querySelectorAll('h5')
-for (let button of buttons) {
-
-
-  button.addEventListener('click', function () {
-    if (button.innerHTML == 'MOSTRAR') {
-      button.innerHTML = 'ESCONDER';
-      button.nextElementSibling.classList.remove('show')
-
-    } else if (button.innerHTML == 'ESCONDER') {
-      button.innerHTML = 'MOSTRAR';
-      button.nextElementSibling.classList.add('show')
-
-    }
-
-
-  })
-}
-
-
-
-function addIngredient() {
-  const ingredients = document.querySelector("#ingredients");
-  const fieldContainer = document.querySelectorAll(".ingredient");
-
-  // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-
-  // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
-
-  // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  ingredients.appendChild(newField);
-}
-
-
-
-function addPrep() {
-  const preps = document.querySelector("#preparation");
-  const fieldContainer = document.querySelectorAll(".prepare");
-
-  // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-
-  // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
-
-  // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  preps.appendChild(newField);
-}
-
-
-document.querySelector(".add-ingredient").addEventListener("click", addIngredient);
-
-
-document.querySelector(".add-step").addEventListener("click", addPrep);
-
 
